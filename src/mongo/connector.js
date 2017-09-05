f45003c7c11c68c08b493bdb93452c4bdf0033fc
Kevin Hermawan <kevinhermawanx@gmail.com>
2017-09-05 11:25:20 +0700

    Add collection

--- src/mongo/connector.js
+++ src/mongo/connector.js
@@ -5,6 +5,7 @@ const MONGO_URL = 'mongodb://localhost:27017/flatnews'
 module.exports = async () => {
   const db = await MongoClient.connect(MONGO_URL)
   return {
-    Links: db.collection('links')
+    Links: db.collection('links'),
+    Users: db.collection('users')
   }
 }
