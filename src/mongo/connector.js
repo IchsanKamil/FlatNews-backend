994719fd210ab24fc31640bcb5011811b25c64f4
Kevin Hermawan <kevinhermawanx@gmail.com>
2017-09-05 14:06:05 +0700

    Overfetching problem

--- src/mongo/connector.js
+++ src/mongo/connector.js
@@ -1,9 +1,17 @@
-const { MongoClient } = require('mongodb')
+const { Logger, MongoClient } = require('mongodb')
 
 const MONGO_URL = 'mongodb://localhost:27017/flatnews'
 
 module.exports = async () => {
   const db = await MongoClient.connect(MONGO_URL)
+
+  let logCount = 0
+  Logger.setCurrentLogger((msg, state) => {
+    console.log(`MONGODB REQUEST ${++logCount}: ${msg}`)
+  })
+  Logger.setLevel('debug')
+  Logger.filter('class', ['Cursor'])
+
   return {
     Links: db.collection('links'),
     Users: db.collection('users'),
